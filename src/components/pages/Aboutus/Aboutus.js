8641b25fc5a3b31bdb0c7ea3c0890e9cd7c1f102
Kunal Bashist <62015640+kunalbashist@users.noreply.github.com>
2021-06-18 01:06:17 +0530

    Update Aboutus.js

--- src/components/pages/Aboutus/Aboutus.js
+++ src/components/pages/Aboutus/Aboutus.js
@@ -6,17 +6,17 @@ function Aboutus() {
     return (
         <>
             <div className="kunal ma4 mh4 bg-dark dib pa2 pv3">
-              <img src="images/kunal.jpg" alt="Kunal"  />
+              <img src="public/images/kunal.jpg" alt="Kunal"  />
               <h1> Kunal Bashist </h1>            
             </div>
 
             <div className="chandani ma4 mh4 bg-dark dib pa2 pv3">
-            <img src="images/chandani.jpg" alt="chandani" />
+            <img src="public/images/chandani.jpg" alt="chandani" />
             <h1> Kumari Chandni </h1>            
           </div>
 
           <div className="abhinav ma4 mh4 bg-dark dib pa2 pv3">
-          <img src="images/abhinav.JPG" alt="Abhinav" />
+          <img src="public/images/abhinav.JPG" alt="Abhinav" />
           <h1> Abhinav Kumar </h1>            
         </div>
        </> 
